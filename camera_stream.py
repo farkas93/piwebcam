1e1f55423a6f2848fb68aa15eae7f93e9fcb7da6
farkas93 <zsombor.kalotay@gmx.ch>
2024-02-06 23:21:26 +0100

    log more

--- camera_stream.py
+++ camera_stream.py
@@ -11,14 +11,17 @@ class CameraStream:
         self.output = None
         self.lock = threading.Lock()
         self.configure_camera()
+        logging.info("INIT CAM")
 
     def configure_camera(self):
         # Configure the camera here, e.g., resolution
         config = self.picam2.create_still_configuration()
         config['main']['size'] = (640, 480)
         self.picam2.configure(config)
+        logging.info("CONFIG CAM")
 
     def capture_frames(self):
+        logging.info("CAPTURE CAM")
         while True:
             with self.lock:
                 # Capture an image to a numpy array, then encode as JPEG

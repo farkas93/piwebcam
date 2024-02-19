c74bd933c32435329366fdaed685e799968c39e6
farkas93 <zsombor.kalotay@gmx.ch>
2024-02-19 18:01:25 +0100

    resnet ftw

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ logging.basicConfig(level=log_level_mapping.get(log_level, logging.WARNING))
 
 
 def main():
-    framerate = 10.0
+    framerate = 5.0
     resolution = RES_480P
     run(framerate=framerate, resolution=resolution, handler_class=StreamingHandler)
 
